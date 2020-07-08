59c93d4f0762b8b1c6f310c96cc5b48c58ba5f30
Maria <mariakhandaker@gmail.com>
2020-07-08 01:57:51 +0000

    it's not working now :(

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,24 @@
 const request = require('request');
-const argument = process.argv.slice(2);
 
-//create get request to return cat data
-//api also has a URL  
-const getDescription = {
-  url: `https://api.thecatapi.com/v1/breeds/search?q=${argument}`,
-  headers: {
-    'User-Agent': 'request',
-    'x-api-key' : 'a074a64c-57fe-4ffe-a8f8-6f5929d74c1a'
-  }
+const fetchBreedDescripition = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, body) {
+    // console.error('error:', error); // Print the error if one occurred
+    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    // console.log('body:', body);
+    // console.log(typeof body);
+    if (error !== null) {
+      callback(error, null);
+    }
+    const data = JSON.parse(body);
+    // console.log(data);
+    // console.log(typeof data);
+    if (data[0] === undefined) {
+      callback(null, "Breed not found.");
+    } else {
+      callback(null, data[0].description);
+    }
+  })
 };
 
-request(getDescription, function (error, response, body) {
-  // console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body);
-  // console.log(typeof body);
-  if (error !== null) {
-    console.log("Error! Request failed.");
-  }
-  const data = JSON.parse(body);
-  // console.log(data);
-  // console.log(typeof data);
-  if (data[0] === undefined) {
-    console.log("Error! Breed not found.");
-  } else {
-    console.log(data[0]["description"]);
-  }
-})
 
+module.exports = { fetchBreedDescripition };
