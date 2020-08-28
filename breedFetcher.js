c1d4ec0ef925c2a297240bb837e82dfa9ae4a7da
Maria Khandaker <mariakhandaker@Marias-MacBook-Pro.local>
2020-08-27 22:08:12 -0400

    don't think i fixed it lol

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,18 @@
 const request = require('request');
+const breedName = process.argv.slice(2);
 
-const fetchBreedDescripition = (breedName, callback) => {
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function (error, body) {
-    // console.error('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body);
-    // console.log(typeof body);
-    if (error !== null) {
+const fetchBreedDescripition = (breedName) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+     if (error !== null) {
       callback(error, null);
     }
     const data = JSON.parse(body);
-    // console.log(data);
-    // console.log(typeof data);
+  
     if (data[0] === undefined) {
       callback(null, "Breed not found.");
-    } else {
-      callback(null, data[0].description);
     }
+    console.log(data[0].description);
   })
 };
-
 
 module.exports = { fetchBreedDescripition };
